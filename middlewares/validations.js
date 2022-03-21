f223b088a88cc26a9c314b9146dafc53dc368745
Diego Silva <coxcorp@gmail.com>
2022-03-21 00:45:54 -0300

    cria validation

---
+++ middlewares/validations.js
@@ -0,0 +1,48 @@
+const displayNameValidation = (req, res, next) => {
+  const { displayName } = req.body;
+  if (displayName.length < 8) {
+    return res.status(400).json(
+      { message: '"displayName" length must be at least 8 characters long' },
+);
+  }
+  next();
+};
+
+const emailValidation = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) return res.status(400).json({ message: '"email" is required' });
+
+  const regexEmail = /\S+@\S+\.\S+/;
+  if (!regexEmail.test(email)) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+  next();
+};
+
+const passwordValidation = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) return res.status(400).json({ message: '"password" is required' });
+  if (password.length < 6) {
+    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+  }
+  next();
+};
+
+const loginValidations = () => {
+  console.log('teste');
+};
+const categoriesValidations = () => {
+  console.log('teste');
+};
+const postValidatins = () => {
+  console.log('teste');
+};
+
+module.exports = {
+  displayNameValidation,
+  emailValidation,
+  passwordValidation,
+  loginValidations,
+  categoriesValidations,
+  postValidatins,
+};
