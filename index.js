efa947ca8b915ea1637924f2502ede7382c18772
Diego Silva <coxcorp@gmail.com>
2022-03-20 22:05:47 -0300

    atualiza index.js

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const error = require('./middlewares/error');
 const app = express();
 app.use(express.json());
 
-app.use('/user', routers.userRouter);
-app.use('/login', routers.loginRouter);
-app.use('/categories', routers.categoriesRouter);
-app.use('/post', routers.postRouter);
-
-app.use(error);
-
-app.listen(process.env.PORT, () => console.log(`ouvindo porta ${process.env.PORT}!`));
+const port = process.env.PORT || 3000;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/login', routers.loginRouter);
+app.use('/user', routers.userRouter);
+app.use('/categories', routers.categoriesRouter);
+app.use('/post', routers.postRouter);
+
+app.use(error);
+
+app.listen(port, () => console.log(`ouvindo porta ${port}!`));
